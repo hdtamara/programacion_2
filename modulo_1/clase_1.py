88c0f272008dbf0ffc1a39c021ce5be6c9d1da53
Hernán Támara O. <hernantama@outlook.es>
2026-09-11 08:56:07 -0500

    Implementa el menú principal para la gestión de contactos

--- modulo_1/clase_1.py
+++ modulo_1/clase_1.py
@@ -2,3 +2,29 @@ nombre = []
 telefono = []
 email = []
 direccion = []
+
+
+while True:
+    menu = """
+    
+    ### ELIGE UNA OPCION ###
+    
+    1. Agregar un contacto.
+    2. Busca un contacto.
+    3. Eliminar un contacto.
+    4. Salir
+    """
+    
+    opcion_elegida = int(input(menu))
+    
+    if opcion_elegida == 1:
+        pass
+    elif opcion_elegida == 2:
+        pass
+    elif opcion_elegida == 3:
+        pass
+    elif opcion_elegida == 4:
+        print("Hasta la vista Baby")
+        break
+    else:
+        print("Opcion invalida intenta nuevamente")
