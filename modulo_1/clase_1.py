e19ded739013202f1308c3a72ae53f51e540a7b9
Hernán Támara O. <hernantama@outlook.es>
2026-09-11 10:16:44 -0500

    Refactor contact management variables and enhance functionality for adding, searching, and displaying contacts

--- modulo_1/clase_1.py
+++ modulo_1/clase_1.py
@@ -1,8 +1,7 @@
-nombre = []
-telefono = []
-email = []
-direccion = []
-
+nombres = []
+telefonos = []
+emails = []
+direcciones = []
 
 while True:
     menu = """
@@ -12,18 +11,46 @@ while True:
     1. Agregar un contacto.
     2. Busca un contacto.
     3. Eliminar un contacto.
-    4. Salir
+    4. Ver Contactos
+    5. Salir
     """
     
     opcion_elegida = int(input(menu))
     
     if opcion_elegida == 1:
-        pass
+        nombre = input("Ingrese el nombre: ")
+        nombre = nombre.lower()
+        telefono = input("Ingrese el telefono: ")
+        email = input("Ingrese su email: ")
+        direccion = input("Ingrese su dirección: ")    
+        nombres.append(nombre)
+        telefonos.append(telefono)
+        emails.append(email)
+        direcciones.append(direccion)
+        print(f"Contacto {nombre} gurdado exitosamente ✅") 
     elif opcion_elegida == 2:
-        pass
+        nombre = input("Ingrese el nombre a buscar: ")
+        if nombre in nombres:
+            indice = nombres.index(nombre.lower())
+            print("="*10)
+            print(f"Nombre: {nombres[indice]}")
+            print(f"Telefono: {telefonos[indice]}")
+            print(f"Email: {emails[indice]}")
+            print(f"Dirección: {direcciones[indice]}")
+            print("="*10)
+            
+        else:
+            print("Contacto no existe")
     elif opcion_elegida == 3:
         pass
     elif opcion_elegida == 4:
+        for nombre,telefono,email,direccion in zip(nombres,telefonos,emails,direcciones):
+            print(f"Nombre: {nombre}")
+            print(f"Telefono: {telefono}")
+            print(f"Email: {email}")
+            print(f"Dirección: {direccion}")
+            print("="*10)
+    elif opcion_elegida == 5:
         print("Hasta la vista Baby")
         break
     else:
